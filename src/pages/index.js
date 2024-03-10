02bf9431c262a0447f6133e046dfd2bba433ee80
Vidya Sagar <59490902+vidya-hub@users.noreply.github.com>
2024-03-10 14:03:45 +0530

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,7 @@ const BlogIndex = ({ data, location }) => {
     return (
       <Layout location={location} title={siteTitle}>
         <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
+        <p>No blog posts found.</p>
       </Layout>
     )
   }
